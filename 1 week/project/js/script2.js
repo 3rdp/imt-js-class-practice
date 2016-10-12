759d78716e5b00f6184a6d1fa07d8edb93f4033f
Sasha Davydenko <third.pancake16@gmail.com>
2016-10-12 21:14:53 +0300

    Конец 1-го занятия

--- 1 week/project/js/script2.js
+++ 1 week/project/js/script2.js
@@ -2,13 +2,27 @@
 Много
 строчный
 */
-alert('Привет, мир!'); // ОДнострочный
+// alert('Привет, мир!'); // ОДнострочный
 
-var name = 'Саша\n',
-	NaMe = 'Name2\n',
-	AGE = 16, // константы капсом
-	surname = 'Давыденко'; // пропускаем строку после объявления - хороший тон
 
-// document.write(name + NaMe + '<br>');
-document.write(name + surname + '<br>');
-document.write('Мне ' + AGE + ' лет<br>');
+// var name = 'Саша\n',
+// 	NaMe = 'Name2\n',
+// 	AGE = 16, // константы капсом
+// 	surname = 'Давыденко'; // пропускаем строку после объявления - хороший тон
+
+// // document.write(name + NaMe + '<br>');
+// document.write(name + surname + '<br>');
+// document.write('Мне ' + AGE + ' лет<br>');
+
+// name = 'Андрей\n';
+// surname = 'Карпенко\n';
+
+// document.write(name + surname + '<br>');
+
+
+var userName = prompt('Введите Ваше имя:', 'Саша'),
+	userSurname = prompt('Введите Ваше имя:', 'Давыденко'),
+	userAge = prompt('Введите Ваш возраст:', '19');
+
+document.write('Добрый день, ' + userName + ' ' + userSurname + '!<br>')
+document.write('Ваш возраст: ' + userAge + '<br />');
