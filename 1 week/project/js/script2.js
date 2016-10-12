3d53cbd24410c0fa5d18159c80d14a049511b188
Sasha Davydenko <third.pancake16@gmail.com>
2016-10-12 20:44:40 +0300

    Просто пробую ещё раз

--- 1 week/project/js/script2.js
+++ 1 week/project/js/script2.js
@@ -3,3 +3,12 @@
 строчный
 */
 alert('Привет, мир!'); // ОДнострочный
+
+var name = 'Саша\n',
+	NaMe = 'Name2\n',
+	AGE = 16, // константы капсом
+	surname = 'Давыденко'; // пропускаем строку после объявления - хороший тон
+
+// document.write(name + NaMe + '<br>');
+document.write(name + surname + '<br>');
+document.write('Мне ' + AGE + ' лет<br>');
