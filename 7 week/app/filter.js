cb88650d3e32c2d9b8256a0fee77f9cc43273b82
3rdp <third.pancake16@gmail.com>
2016-11-30 21:33:11 +0200

    Конец 14-го занятия

--- 7 week/app/filter.js
+++ 7 week/app/filter.js
@@ -1,5 +1,8 @@
 window.onload = function () {
 	(function(){
+		var globalTest = '123'; // без var глобальная переменная переписалась бы
+		console.log('globalTest', globalTest);
+		
 		var inputElems = document.getElementsByTagName('input');
 
 		for (var i = inputElems.length - 1; i >= 0; i--) {
@@ -12,9 +15,23 @@ window.onload = function () {
 
 		function filter(event) {
 			var target = event.target,
-				key = event.key,
+				key,
+				code,
 				allowed = target.getAttribute('data-allowed-chars'),
 				messageId = target.getAttribute('data-messageid');
+
+			// if (event.type.toLowerCase() === 'textinput' ) {
+			// 	key = event.data;
+			// } else {
+				// для firefox
+				code = event.charCode || event.keyCode;
+
+				if (code < 32 || event.ctrlKey || event.altKey) {
+					return;
+				}
+
+				key = String.fromCharCode(code);
+			// }
 
 			if (messageId) 
 				var messageElem = document.getElementById(messageId);
