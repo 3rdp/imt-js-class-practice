2728f34afea684cbffecc0440112d393980e1d77
3rdp <third.pancake16@gmail.com>
2016-12-07 19:11:26 +0200

    Homework async XHR with native promise

--- 7 week/app2/login.js
+++ 7 week/app2/login.js
@@ -9,7 +9,42 @@ window.onload = function() {
 		var login,
 			pass,
 			ajaxServiceObj = ajaxService(),
-			arrUsers = ajaxServiceObj.getJSON('./auth.json').users;
+			// arrUsers = ajaxServiceObj.getJSON('./auth.json').users;
+			arrUsers = ajaxServiceObj.getJSONasync('./auth.json', function(res) {
+				return res.users;
+			});
+			arrUsers,
+			arrUsersPromise = new Promise(function(resolve, reject) {
+				return ajaxServiceObj.getJSONasync('./auth.json', function(res) {
+					resolve(res.users);
+				});
+			})
+			
+		arrUsersPromise.then(result => {
+			arrUsers = result;
+
+			login = document.getElementById('login').value;
+			pass = document.getElementById('login-pass').value;
+
+			var check;
+			arrUsers.forEach(function(v) {
+				if (login == v.name && pass == v.age) {
+					alert('Успешная авторизация');
+					window.sessionStorage.setItem('auth', true);
+					window.location.replace('index.html');
+					check = true;
+				}
+			})
+			
+			if (!check) {
+				document.getElementById('login-error').innerHTML = 'Неверные данные!';
+
+				setTimeout(function(){
+					document.getElementById('login-error').innerHTML = ''
+				}, 3000);
+				
+			}
+		})
 
 		login = document.getElementById('login').value;
 		pass = document.getElementById('login-pass').value;
--- 7 week/app2/services/ajax_service.js
+++ 7 week/app2/services/ajax_service.js
@@ -1,31 +1,51 @@
 function ajaxService(argument) { 
 	return { // сам сервис
 		getJSON: _getJSON, // Синхронно получаем объект из JSON файла
-		postJSON: _postJSON
+		postJSON: _postJSON,
+		getJSONasync: _getJSONasync
+	}
+
+	function _getJSON(url) {
+		var xhr = new XMLHttpRequest();
+
+		// xhr.onreadystatechange = function XHRonReady(res) {
+		// 	if (xhr.readyState === 4 && xhr.status == 200) {
+		// 		console.info('DONE');
+		// 		try {
+		// 			var result = JSON.parse(xhr.response);
+		// 			console.log('result', result);
+		// 			return result;
+		// 		} catch(e) {
+		// 			console.log("Структура ответа не JSON", e);
+		// 		}
+		// 	}
+		// }
+		// xhr.open('GET', url, true);
+		xhr.open('GET', url, false);
+		xhr.send(null);
+		return JSON.parse(xhr.response);
+	}
+
+	function _postJSON(url) {
+		return null;
+	}
+
+	function _getJSONasync(url, callback) {
+		var xhr = new XMLHttpRequest();
+		
+		xhr.onreadystatechange = function XHRonReady(res) {
+			if (xhr.readyState === 4 && xhr.status == 200) {
+				console.info('DONE');
+				try {
+					var result = JSON.parse(xhr.response);
+					callback(result);
+				} catch(e) {
+					console.log("Структура ответа не JSON", e);
+				}
+			}
+		}
+		xhr.open('GET', url, true);
+		xhr.send(null);
 	}
 }
 
-function _getJSON(url) {
-	var xhr = new XMLHttpRequest();
-
-	// xhr.onreadystatechange = function XHRonReady(res) {
-	// 	if (xhr.readyState === 4 && xhr.status == 200) {
-	// 		console.info('DONE');
-	// 		try {
-	// 			var result = JSON.parse(xhr.response);
-	// 			console.log('result', result);
-	// 			return result;
-	// 		} catch(e) {
-	// 			console.log("Структура ответа не JSON", e);
-	// 		}
-	// 	}
-	// }
-	// xhr.open('GET', url, true);
-	xhr.open('GET', url, false);
-	xhr.send(null);
-	return JSON.parse(xhr.response);
-}
-
-function _postJSON(url) {
-	return null;
-}
