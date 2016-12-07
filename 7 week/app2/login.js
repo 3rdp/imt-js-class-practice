f93c6b3f74b3c01e1fcaa6709b3ee6f2d0742118
3rdp <third.pancake16@gmail.com>
2016-12-07 19:20:16 +0200

    Refactor 7 week/app2

--- 7 week/app2/login.js
+++ 7 week/app2/login.js
@@ -2,27 +2,8 @@
 window.onload = function() {
 	console.info('Script login started');
 
-	var btn = document.getElementById('login-btn');
-
-	btn.onclick = function() {
-		console.info('Сработал клик!');
-		var login,
-			pass,
-			ajaxServiceObj = ajaxService(),
-			// arrUsers = ajaxServiceObj.getJSON('./auth.json').users;
-			arrUsers = ajaxServiceObj.getJSONasync('./auth.json', function(res) {
-				return res.users;
-			});
-			arrUsers,
-			arrUsersPromise = new Promise(function(resolve, reject) {
-				return ajaxServiceObj.getJSONasync('./auth.json', function(res) {
-					resolve(res.users);
-				});
-			})
-			
-		arrUsersPromise.then(result => {
-			arrUsers = result;
-
+	var btn = document.getElementById('login-btn'),
+		authorize = function(arrUsers) {
 			login = document.getElementById('login').value;
 			pass = document.getElementById('login-pass').value;
 
@@ -44,28 +25,30 @@ window.onload = function() {
 				}, 3000);
 				
 			}
-		})
+		};
 
-		login = document.getElementById('login').value;
-		pass = document.getElementById('login-pass').value;
 
-		var check;
-		arrUsers.forEach(function(v) {
-			if (login == v.name && pass == v.age) {
-				alert('Успешная авторизация');
-				window.sessionStorage.setItem('auth', true);
-				window.location.replace('index.html');
-				check = true;
-			}
-		})
-		
-		if (!check) {
-			document.getElementById('login-error').innerHTML = 'Неверные данные!';
 
-			setTimeout(function(){
-				document.getElementById('login-error').innerHTML = ''
-			}, 3000);
+	btn.onclick = function() {
+		console.info('Сработал клик!');
+		var login,
+			pass,
+			ajaxServiceObj = ajaxService(),
+			// arrUsers = ajaxServiceObj.getJSON('./auth.json').users;
+			arrUsers = ajaxServiceObj.getJSONasync('./auth.json', function(res) {
+				return res.users;
+			}),
+			arrUsers,
+			arrUsersPromise = new Promise(function(resolve, reject) {
+				return ajaxServiceObj.getJSONasync('./auth.json', function(res) {
+					resolve(res.users);
+				});
+			});
 			
-		}
+		arrUsersPromise.then(result => {
+			authorize(result);	
+		})
+
+		authorize(arrUsers);
 	}
 }
--- 7 week/app2/services/ajax_service.js
+++ 7 week/app2/services/ajax_service.js
@@ -8,19 +8,6 @@ function ajaxService(argument) {
 	function _getJSON(url) {
 		var xhr = new XMLHttpRequest();
 
-		// xhr.onreadystatechange = function XHRonReady(res) {
-		// 	if (xhr.readyState === 4 && xhr.status == 200) {
-		// 		console.info('DONE');
-		// 		try {
-		// 			var result = JSON.parse(xhr.response);
-		// 			console.log('result', result);
-		// 			return result;
-		// 		} catch(e) {
-		// 			console.log("Структура ответа не JSON", e);
-		// 		}
-		// 	}
-		// }
-		// xhr.open('GET', url, true);
 		xhr.open('GET', url, false);
 		xhr.send(null);
 		return JSON.parse(xhr.response);
